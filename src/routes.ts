50fe304b963fb716fb31001ae6cce563858e8f8b
Miguel <miguelcg958@gmail.com>
2022-01-09 00:52:51 -0300

    Implement POST /posts

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,12 @@ import { Router } from 'express';
 
 import { prisma } from './database';
 
+interface CreatePostInput {
+  content?: string;
+  validator?: string;
+  createdAt?: string;
+}
+
 const routes = Router();
 
 routes.get('/posts', async (request, response) => {
@@ -23,8 +29,28 @@ routes.get('/posts', async (request, response) => {
   });
 });
 
-routes.post('/posts', (request, response) => {
-  return response.json(null);
+routes.post('/posts', async (request, response) => {
+  const { content, validator, createdAt } = request.body as CreatePostInput;
+
+  if (!content || !validator)
+    return response.status(400).json({ error: 'Missing content or validator' });
+
+  let createdAtDate: Date | undefined = undefined;
+
+  if (createdAt) createdAtDate = new Date(createdAt);
+
+  if (validator !== process.env.VALIDATOR)
+    return response.status(401).json({ error: 'Invalid validator' });
+
+  const post = await prisma.post.create({
+    data: {
+      content,
+      createdAt: createdAtDate,
+      updatedAt: createdAtDate,
+    },
+  });
+
+  return response.status(201).json(post);
 });
 
 export { routes };
