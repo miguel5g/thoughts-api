e5be21d8e663404c365c0f0c3423a7234ed1ac31
Miguel <miguelcg958@gmail.com>
2022-01-09 01:08:14 -0300

    Updated default PORT

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 import { app } from './app';
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}/`);
