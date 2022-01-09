277d2b0b7ad2aa6daf4728c79f568422ee313c7e
Miguel <miguelcg958@gmail.com>
2022-01-09 00:32:50 -0300

    Implemented GET /posts

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express';
 
+import { prisma } from './database';
+
 const routes = Router();
 
-routes.get('/posts', (request, response) => {
-  return response.json(null);
+routes.get('/posts', async (request, response) => {
+  const { page } = request.query;
+
+  const parsedPage = parseInt(page as string) || 1;
+  const postLength = await prisma.post.count();
+  const posts = await prisma.post.findMany({
+    skip: parsedPage * 10 - 10,
+    take: 10,
+  });
+
+  return response.json({
+    total: postLength,
+    limit: 10,
+    page: parsedPage,
+    pages: Math.floor(postLength / 10) + 1,
+    data: posts,
+  });
 });
 
 routes.post('/posts', (request, response) => {
